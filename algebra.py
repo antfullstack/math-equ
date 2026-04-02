5c28fd7fc9b653daa0f73845bd7b6bf634bafc20
antfullstack <253273528+antfullstack@users.noreply.github.com>
2026-04-02 16:58:33 +0300

    Closes #12: Add GCD function with two numbers.

--- algebra.py
+++ algebra.py
@@ -1,12 +1,17 @@
 def power(base, exponent):
     return base ** exponent
 
+#Takes a number and until it is equal to 0 it multiplies it with its previous in order storing the new result.
 def factorial(number):
-    #Takes a number and until it is equal to 0 it multiplies it with its previous in order storing the new result
     result = 1
     while number > 0:
         result *= number
         number -= 1    
     return result
 
-print(factorial(5))
+#Implementation based on the Eucledean algorithm.
+def gcd(a, b):
+    if b == 0: 
+        return a
+    return gcd(b, a % b)
+
