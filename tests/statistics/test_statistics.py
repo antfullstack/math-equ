f1ef111f5a4915cc3c333b38a65c5f714250d131
antfullstack <253273528+antfullstack@users.noreply.github.com>
2026-04-12 22:38:29 +0300

    Update test_statistics.py

--- tests/statistics/test_statistics.py
+++ tests/statistics/test_statistics.py
@@ -4,7 +4,7 @@ import sys
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(PROJECT_ROOT))
-from simple_equ.statistics.statistics import mode, standardization, average, median, percentage, linear_regression, dot
+from simple_equ.statistics.statistics import mode, standardization, average, median, percentage, linear_regression, dot, bayes_theorem
 
 
 # ==============================================================================
